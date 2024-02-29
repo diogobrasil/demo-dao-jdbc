b02d7d7022caa24d6de28b2cf5381588a2dccee6
diogo <diogobrasildasilva@gmail.com>
2024-02-29 15:41:19 -0300

    DepartmentDao implementation and test

--- src/application/Program1.java
+++ src/application/Program1.java
@@ -10,7 +10,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-public class Program {
+public class Program1 {
     public static void main(String[] args){
 
         Locale.setDefault(Locale.US);
@@ -22,7 +22,7 @@ public class Program {
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
-        System.out.println("\n=== TEST 2: seller findById ===");
+        System.out.println("\n=== TEST 2: seller findByDepartment ===");
         Department department = new Department(2, null);
         List<Seller> list = sellerDao.findByDepartment(department);
         for(Seller obj : list){
