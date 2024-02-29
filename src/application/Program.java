dc91cd5ee383274560b9dfdd81becce2b857614d
diogo <diogobrasildasilva@gmail.com>
2024-02-28 23:44:11 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,11 @@
 package application;
+
+import model.entities.Department;
+
 public class Program {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
     }
 }
